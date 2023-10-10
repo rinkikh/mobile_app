a6d05e76c93350bc4413eec9d97d753cce763588
rinki <rinki9414@gmail.com>
2023-10-10 10:41:31 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase,ref,push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 
 
@@ -8,8 +8,8 @@ const appSettings = {
     databaseURL: "https://mycart-af0b7-default-rtdb.firebaseio.com/"
 }
 const app = initializeApp(appSettings)
-const database =getDatabase(app)
-const itemsInDB = ref(database,"items")
+const database = getDatabase(app)
+const itemsInDB = ref(database, "items")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
@@ -20,8 +20,48 @@ addButtonEl.addEventListener("click", function () {
 
     push(itemsInDB, inputValue)
 
-    inputFieldEl.value =""
+    clearInputFieldEl()
 
-    shoppingListEl.innerHTML +=`<li>${inputValue}</li>`
-    console.log(inputValue)
 })
+
+onValue(itemsInDB, function (snapshot) {
+    let itemsArray = Object.entries(snapshot.val())
+
+    clearShoppingListEl()
+
+
+
+    for (let i = 0; i < itemsArray.length; i++) {
+        let currentItem = itemsArray[i]
+        let currentItemId = currentItem[0]
+        let currentItemValue = currentItem[1]
+        appendItemToShoppingListEl(currentItem)
+    }
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+
+}
+
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+
+}
+
+function appendItemToShoppingListEl(item) {
+    // shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+    let itemId = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+    newEl.textContent = itemValue
+
+    newEl.addEventListener("click", function () {
+        let exactLocationOfItemInDB = ref(database, `items/${itemId}`)
+        remove(exactLocationOfItemInDB)
+    })
+
+    shoppingListEl.append(newEl)
+}
